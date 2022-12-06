5ec808192a36182ae9523fd8be4b27062eb7eca9
Morazzer <morazzer@morazzer.dev>
2022-12-06 22:03:34 +0100

    Added day3

---
+++ days/day3.go
@@ -0,0 +1,75 @@
+package days
+
+import (
+	"bufio"
+	_ "embed"
+	"fmt"
+	"strings"
+)
+
+//go:embed inputs/day3.txt
+var input3 string
+
+func Day3() {
+	input := input3
+
+	var priority int
+	priority = 0
+
+	charMapping := make(map[rune]int)
+
+	for i := 1; i <= 26; i++ {
+		charMapping[rune(97+i-1)] = i
+		charMapping[rune(65+i-1)] = i + 26
+	}
+
+	scannner := bufio.NewScanner(strings.NewReader(input))
+	for scannner.Scan() {
+		line := scannner.Text()
+		first, second := line[0:len(line)/2], line[len(line)/2:]
+		for _, firstChar := range first {
+			for _, secondChar := range second {
+				if (firstChar == secondChar) && (strings.ContainsRune(first, firstChar) && strings.ContainsRune(second, secondChar)) {
+					first = strings.ReplaceAll(first, string(firstChar), "")
+					second = strings.ReplaceAll(second, string(firstChar), "")
+					priority += charMapping[firstChar]
+					break
+				}
+			}
+		}
+	}
+
+	fmt.Printf("Solution: %d", priority)
+}
+
+func Day3_2() {
+	input := input3
+
+	var priority int
+	priority = 0
+
+	charMapping := make(map[rune]int)
+
+	for i := 1; i <= 26; i++ {
+		charMapping[rune(97+i-1)] = i
+		charMapping[rune(65+i-1)] = i + 26
+	}
+
+	scannner := bufio.NewScanner(strings.NewReader(input))
+	for scannner.Scan() {
+		line1 := scannner.Text()
+		scannner.Scan()
+		line2 := scannner.Text()
+		scannner.Scan()
+		line3 := scannner.Text()
+
+		for _, rune := range line1 {
+			if strings.ContainsRune(line2, rune) && strings.ContainsRune(line3, rune) {
+				priority += charMapping[rune]
+				break
+			}
+		}
+	}
+
+	fmt.Printf("Solution: %d", priority)
+}
